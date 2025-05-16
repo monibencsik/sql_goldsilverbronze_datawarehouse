f98cbdc7d0261e4b05941ae9f9cbbab0c99d8373
monibencsik <97840088+monibencsik@users.noreply.github.com>
2025-05-16 22:34:14 +0100

    Add files via upload
    comments updated

--- scripts/silver_table2insert.sql
+++ scripts/silver_table2insert.sql
@@ -1,31 +1,36 @@
--- Loading silver.crm_prd_info
+/*
+---------------------------------------------------------------------
+Raw data from 
+bronze.crm_prd_info 
+was cleaned, derived columns were prepared, and the transformed dataset was loaded into 
+silver.crm_prd_info
+---------------------------------------------------------------------
+*/
 
-		INSERT INTO silver.crm_prd_info (
-			prd_id,
-			cat_id,
-			prd_key,
-			prd_nm,
-			prd_cost,
-			prd_line,
-			prd_start_dt,
-			prd_end_dt
-		)
-		SELECT
-			prd_id,
-			REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id, -- Extract category ID
-			SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,        -- Extract product key
-			prd_nm,
-			ISNULL(prd_cost, 0) AS prd_cost,
-			CASE 
-				WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
-				WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
-				WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
-				WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
-				ELSE 'n/a'
-			END AS prd_line, -- Map product line codes to descriptive values
-			CAST(prd_start_dt AS DATE) AS prd_start_dt,
-			CAST(
-				LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 
-				AS DATE
-			) AS prd_end_dt -- Calculate end date as one day before the next start date
-		FROM bronze.crm_prd_info;
+TRUNCATE TABLE silver.crm_prd_info;
+INSERT INTO silver.crm_prd_info (
+	prd_id,
+	cat_id,
+	prd_key,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt
+)
+SELECT
+	prd_id,
+	replace(left(prd_key,5), '-', '_') as cat_id,
+	mid(prd_key, 7, LENgth(prd_key)) as prd_key,
+	prd_nm,
+	ifnull(prd_cost, 0) as prd_cost,
+	CASE prd_line
+		WHEN 'M' THEN 'Mountain'
+		WHEN 'R' THEN 'Road'
+		WHEN 'S' THEN 'Other Sales'
+		WHEN 'T' THEN 'Touring'
+		ELSE 'n/a'
+	END AS prd_line,
+	CAST(prd_start_dt AS DATE) AS prd_start_dt,
+    CAST(DATE_SUB(LEAD(prd_start_dt) OVER (PARTITION BY prd_nm order by prd_start_dt), INTERVAL 1 DAY) AS DATE) as prd_end_dt
+FROM bronze.crm_prd_info;
--- scripts/silver_tables.sql
+++ scripts/silver_tables.sql
@@ -21,12 +21,13 @@ CREATE TABLE if not exists silver.crm_cust_info (
 DROP TABLE if exists silver.crm_prd_info;
 CREATE TABLE if not exists silver.crm_prd_info (
     prd_id       INT,
+    cat_id		 VARCHAR(50),
     prd_key      VARCHAR(50),
     prd_nm       VARCHAR(50),
     prd_cost     INT,
     prd_line     VARCHAR(50),
-    prd_start_dt DATETIME,
-    prd_end_dt   DATETIME,
+    prd_start_dt DATE,
+    prd_end_dt   DATE,
     dwh_curdate	DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
